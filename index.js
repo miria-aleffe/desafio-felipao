d90795c8e536446f95462e3d6c61ef5f547a2640
miria <miria_aleffe@hotmail.com>
2024-01-30 13:28:42 -0300

    finalizando o projeto

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let nomeDoHeroi = "Mulher Maravilha"
-let xpDoHeroi = "8000"
-let nivel = ""
+let xpDoHeroi = "8001"
+let nivel = " "
 
-if (xpDoHeroi > "1000" && xpDoHeroi <= "2000"){
+if (xpDoHeroi > "1000" && xpDoHeroi <= "2001"){
     nivel = "Ferro"
-}else if (xpDoHeroi > "2001" && xpDoHeroi <= "3000"){
+}else if (xpDoHeroi > "2001" && xpDoHeroi <= "3001"){
     nivel = "Bronze"
-}else if (xpDoHeroi <= "3001"){
+}else if (xpDoHeroi > "3001" && xpDoHeroi <= "4001"){
     nivel = "Prata"
-}else if (xpDoHeroi <= "4001"){
+}else if (xpDoHeroi > "4001" && xpDoHeroi <= "5001"){
     nivel = "Ouro"
-}else if (xpDoHeroi <= "5001"){
+}else if (xpDoHeroi > "5001" && xpDoHeroi <= "6001"){
     nivel = "Platina"
-}else if (xpDoHeroi <= "6001"){
+}else if (xpDoHeroi > "6001" && xpDoHeroi <= "7001"){
     nivel = "Ascendente"
-}else if (xpDoHeroi <= "7001"){
+}else if (xpDoHeroi > "7001" && xpDoHeroi <= "8001"){
     nivel = "Imortal"
-}else if(xpDoHeroi <= "10001"){
+}else if(xpDoHeroi > "8001"){
     nivel = "Radiante"
 }
 
